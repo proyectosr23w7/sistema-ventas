b905e37b5fe8ab5ebcbe074ab9422016f181c40e
proyectosr23w7 <proyectosr23w7@gmail.com>
2025-12-19 14:56:15 -0400

    Actualización desde casa cpanel

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,44 @@
+<?php return array (
+  'inertiajs/inertia-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Inertia\\ServiceProvider',
+    ),
+  ),
+  'laravel/fortify' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Fortify\\FortifyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/wayfinder' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Wayfinder\\WayfinderServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+);
